5f59a4a2692e90576d7ba008da964930c49729bf
enesGune <enes_1244_@hotmail.com>
2021-06-11 20:57:26 +0300

    8:58

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default class ProductSevice {
   getProducts() {
     return axios.get("https://jsonplaceholder.typicode.com/posts");
-    // şuan apiye axios ile sorgu atıyoruz
+    // şuan apiye axios ile sorgu atıyoruz 2:37
   }
 }
